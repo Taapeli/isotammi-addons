ae834af87dcfb4a24b770f75c69ea290aab98e17
Kari Kujansuu <karikujansuu@gmail.com>
2021-08-02 22:04:12 +0300

    Update packages

--- source/SuperTool/SuperTool.gpr.py
+++ source/SuperTool/SuperTool.gpr.py
@@ -32,7 +32,7 @@ plug.tool.tool_categories["Isotammi"] = ("Isotammi", _("Isotammi tools"))
 # SuperTool
 #
 # ------------------------------------------------------------------------
-VERSION="1.1.0"
+VERSION="1.1.2"
  
 register(
     TOOL,
--- source/build.sh
+++ source/build.sh
@@ -1,1 +1,2 @@
+rm SuperTool/*.script
 python3.6 build.py
