822f6d5a13fe5c6bad83bdb435ae93319cdcdb12
Kari Kujansuu <kari.kujansuu@gmail.com>
2022-03-15 15:36:15 +0200

    SuperTool version 1.2.1

--- source/SuperTool/SuperTool.gpr.py
+++ source/SuperTool/SuperTool.gpr.py
@@ -32,7 +32,7 @@ plug.tool.tool_categories["Isotammi"] = ("Isotammi", _("Isotammi tools"))
 # SuperTool
 #
 # ------------------------------------------------------------------------
-VERSION="1.2.0"
+VERSION="1.2.1"
  
 register(
     TOOL,
