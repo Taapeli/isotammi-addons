8874ee4b174d557a4ac97fa7712b6413a85aa24a
Kari Kujansuu <kari.kujansuu@gmail.com>
2023-12-06 21:40:40 +0200

    Filter+: add birth and death places in Person filter

--- source/Filter+/filter+.gpr.py
+++ source/Filter+/filter+.gpr.py
@@ -1,3 +1,31 @@
+#
+# Gramps - a GTK+/GNOME based genealogy program
+#
+# Copyright (C) 2023 Gramps developers, Kari Kujansuu
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+
+
+"""
+Gramps registration file
+"""
+
+# See https://github.com/Taapeli/isotammi-addons/tree/master/source/Filter+
+# or https://gramps-project.org/wiki/index.php/Addon:Isotammi_addons#Filter.2B
+
 from gramps.version import major_version, VERSION_TUPLE
 
 objs = """
@@ -12,11 +40,8 @@ Media
 Note
 """
 
-if VERSION_TUPLE < (5, 2, 0):
-    additional_args = {}
-else:
-    additional_args = {"help_url": "https://github.com/Taapeli/isotammi-addons/tree/master/source/Filter+"}
-    
+help_url = "https://gramps-project.org/wiki/index.php/Addon:Isotammi_addons#Filter.2B"
+
 for obj in objs.splitlines():
     obj = obj.strip()
     if obj == "": continue
@@ -24,7 +49,7 @@ for obj in objs.splitlines():
              id=obj + "-Filter+",
              name=_(obj + " Filter+"),
              description = _("Gramplet providing a filter (enhanced)"),
-             version="1.0.2",
+             version="1.0.3",
              gramps_target_version=major_version,
              status = STABLE,
              fname="filter+.py",
@@ -32,14 +57,19 @@ for obj in objs.splitlines():
              gramplet = obj + 'FilterPlus',
              gramplet_title=_("Filter+"),
              navtypes=[obj],
-            **additional_args,
+             help_url=help_url,
      )
+
+if VERSION_TUPLE < (5, 2, 0):
+    additional_args = {}
+else:
+    additional_args = {"help_url": help_url}
 
 register(RULE,
   id    = 'HasEventBase+',
   name  = _("HasEventBase+"),
   description = _("HasEventBase+ - used by Filter+"),
-  version = '1.0.2',
+  version = '1.0.3',
   authors = ["Kari Kujansuu"],
   authors_email = ["kari.kujansuu@gmail.com"],
   gramps_target_version = major_version,
@@ -47,13 +77,14 @@ register(RULE,
   fname = "filter+.py",
   ruleclass = 'HasEventBasePlus',  # rule class name
   namespace = 'Event',  # one of the primary object classes
+  **additional_args,
 )
 
 register(RULE,
   id    = 'HasSourceBase+',
   name  = _("HasSourceBase+"),
   description = _("HasSourceBase+ - used by Filter+"),
-  version = '1.0.2',
+  version = '1.0.3',
   authors = ["Kari Kujansuu"],
   authors_email = ["kari.kujansuu@gmail.com"],
   gramps_target_version = major_version,
@@ -61,5 +92,6 @@ register(RULE,
   fname = "filter+.py",
   ruleclass = 'HasSourceBasePlus',  # rule class name
   namespace = 'Source',  # one of the primary object classes
+  **additional_args,
 )
 
--- source/Filter+/filter+.py
+++ source/Filter+/filter+.py
@@ -1,3 +1,23 @@
+#
+# Gramps - a GTK+/GNOME based genealogy program
+#
+# Copyright (C) 2023 Gramps developers, Kari Kujansuu
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+
 import time
 
 from gi.repository import Gtk
@@ -14,19 +34,22 @@ from gramps.gui.filters.sidebar import (
     MediaSidebarFilter, RepoSidebarFilter, NoteSidebarFilter)
 
 from gramps.plugins.gramplet.filter import Filter
-from gramps.gen.filters.rules.event._hasdata import HasData
-from gramps.gen.filters.rules.citation._hassource import HasSource
+from gramps.gen.filters.rules.event import HasData
+from gramps.gen.filters.rules.citation import HasSource
 from gramps.gen.filters.rules._haseventbase import HasEventBase
 from gramps.gen.filters.rules._hassourcebase import HasSourceBase
-from gramps.gui.filters._searchbar import SearchBar
+from gramps.gui.filters import SearchBar
 from gramps.gen.filters.rules.person import RegExpName
 from gramps.gen.filters import GenericFilter
 from gramps.gen.filters.rules._rule import Rule
 from gramps.gen.filters._genericfilter import GenericFamilyFilter
 from gramps.gen.filters.rules.family._memberbase import father_base, mother_base
-from gramps.gen.filters.rules.family._regexpfathername import RegExpFatherName
-from gramps.gen.filters.rules.family._regexpmothername import RegExpMotherName
-from gramps.gen.filters.rules.family._regexpchildname import RegExpChildName
+from gramps.gen.filters.rules.family import RegExpFatherName
+from gramps.gen.filters.rules.family import RegExpMotherName
+from gramps.gen.filters.rules.family import RegExpChildName
+from gramps.gen.filters.rules.person import HasBirth
+from gramps.gen.filters.rules.person import HasDeath
+from gramps.version import VERSION_TUPLE
 
 _ = glocale.translation.gettext
     
@@ -36,7 +59,8 @@ _ = glocale.translation.gettext
 #
 #-------------------------------------------------------------------------
 class SidebarFilterBase:
-
+    
+    
     def _init_interface(self):
         super()._init_interface()
         b = Gtk.Button(_("Define filter"))
@@ -51,11 +75,41 @@ class SidebarFilterBase:
         self.msg_label.set_halign( Gtk.Align.START)
         self.msg_label.set_margin_left(12)
 
+        if isinstance(self, PersonSidebarFilter):
+            self.add_place_fields()
+
         if isinstance(self, CitationSidebarFilter):
             self.set_min_conf_to_very_low()
-                    
+
+        self.update_reset_function()
+                            
         self.vbox.pack_start(hbox, False, False, 0)
         self.vbox.pack_start(self.msg_label, False, False, 0)
+
+    def add_place_fields(self):
+        # add fields for birth and death places
+        grid = self.vbox.get_children()[0]
+
+        # move existing fields downwards
+        for c in list(grid.get_children()):
+            child_top = grid.child_get_property(c, "top-attach")
+            if child_top == 5:
+                grid.child_set_property(c, "top-attach", child_top+1)
+            if child_top >= 6:
+                grid.child_set_property(c, "top-attach", child_top+2)
+        
+        # add new fields        
+        label_birth_place = Gtk.Label(_("Birth Place"))
+        label_birth_place.set_halign(Gtk.Align.START)
+        grid.attach(label_birth_place, 1, 5, 1, 1)
+        self.entry_birth_place = Gtk.Entry()
+        grid.attach(self.entry_birth_place, 2, 5, 1, 1)
+                
+        label_death_place = Gtk.Label(_("Death Place"))
+        label_death_place.set_halign(Gtk.Align.START)
+        grid.attach(label_death_place, 1, 7, 1, 1)
+        self.entry_death_place = Gtk.Entry()
+        grid.attach(self.entry_death_place, 2, 7, 1, 1)
 
     def set_min_conf_to_very_low(self):
         grid = self.vbox.get_children()[0]
@@ -68,8 +122,47 @@ class SidebarFilterBase:
                     if text == _("Normal"):                
                         c.set_active(0)
 
+    
+    def update_reset_function(self):
+        buttonbox = self.vbox.get_children()[-1]
+        resetbutton = buttonbox.get_children()[1]
+        resetbutton.connect("clicked", self.reset_clicked)
+    
+    def reset_clicked(self, _widget):
+        self.entry_birth_place.set_text("")
+        self.entry_death_place.set_text("")
+
     def get_filter(self):
         the_filter = super().get_filter()
+
+        if isinstance(self, PersonSidebarFilter):
+            if the_filter is None:
+                the_filter = GenericFilter()
+                has_data = False
+            else:
+                has_data = True
+
+            use_regex = self.filter_regex.get_active()
+            if VERSION_TUPLE < (5, 2, 0):
+                args = {}
+            else:
+                use_case = self.sensitive_regex.get_active()
+                args = {"use_case": use_case}
+
+            place = self.entry_birth_place.get_text().strip()
+            if place:
+                the_filter.add_rule(HasBirth(['',place,''], use_regex=use_regex, **args))
+                has_data = True
+
+            place = self.entry_death_place.get_text().strip()
+            if place:
+                the_filter.add_rule(HasDeath(['',place,''], use_regex=use_regex, **args))
+                has_data = True
+            if has_data:
+                return the_filter
+            else:
+                return None
+
         if the_filter is None:
             return None
 
@@ -101,7 +194,7 @@ class SidebarFilterBase:
         # fix some rules:
         new_rules = []
         for rule in the_filter.get_rules():
-            print("rule:", rule.__class__.__name__)
+            # print("rule:", rule.__class__.__name__)
             # HasEvent is defined as an alias for HasEventBase in gramps.gen.filters.rules.event.__init__
             # and the rule class will be HasEventBase.
             # But the code parsing custom_filters.xml does not accept HasEventBase because it is not listed
@@ -210,6 +303,11 @@ class HasSourceBasePlus(HasSourceBase):
     pass
 
 
+#-------------------------------------------------------------------------
+#
+# NameMatch
+#
+#-------------------------------------------------------------------------
 class NameMatch(Rule):
     """Rule that checks for full or partial name matches"""
  
@@ -231,6 +329,11 @@ class NameMatch(Rule):
                 return False
         return True
     
+#-------------------------------------------------------------------------
+#
+# FatherNameMatch
+#
+#-------------------------------------------------------------------------
 class FatherNameMatch(RegExpName):
     """Rule that checks for full or partial name matches"""
 
@@ -242,6 +345,11 @@ class FatherNameMatch(RegExpName):
     base_class = RegExpName
     apply = father_base
     
+#-------------------------------------------------------------------------
+#
+# MotherNameMatch
+#
+#-------------------------------------------------------------------------
 class MotherNameMatch(RegExpName):
     """Rule that checks for full or partial name matches"""
 
