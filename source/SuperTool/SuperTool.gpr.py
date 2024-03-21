0b2475e286364b2439d1250792c2222a33e4238a
Kari Kujansuu <kari.kujansuu@gmail.com>
2024-03-21 15:41:35 +0200

    Update gpr.py files

--- source/SuperTool/SuperTool.gpr.py
+++ source/SuperTool/SuperTool.gpr.py
@@ -32,7 +32,7 @@ plug.tool.tool_categories["Isotammi"] = ("Isotammi", _("Isotammi tools"))
 # SuperTool
 #
 # ------------------------------------------------------------------------
-VERSION="1.4.0"
+VERSION="1.4.1"
  
 
 if VERSION_TUPLE < (5, 2, 0):
